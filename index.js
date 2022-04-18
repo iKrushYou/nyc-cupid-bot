e2bea154c5e35301a5a0e7c7811680edd4af32ef
iKrushYou <akrush95@gmail.com>
2022-04-18 15:58:25 -0400

    bot will now check for max available date (before or on wedding date) and retrieve time slots from there

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ const api_url = "https://clerkscheduler.cityofnewyork.us/s/sfsites/aura?r=10&aur
 const headers = {
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
 }
-const selectedDate = DateTime.now().setZone('America/New_York').toFormat("yyyy-LL-dd")
-const data = `message=%7B%22actions%22%3A%5B%7B%22id%22%3A%2289%3Ba%22%2C%22descriptor%22%3A%22aura%3A%2F%2FApexActionController%2FACTION%24execute%22%2C%22callingDescriptor%22%3A%22UNKNOWN%22%2C%22params%22%3A%7B%22namespace%22%3A%22%22%2C%22classname%22%3A%22SCHED_BookAppointmentController%22%2C%22method%22%3A%22getSlots%22%2C%22params%22%3A%7B%22isDeviceMobile%22%3Afalse%2C%22isCeremonyFlow%22%3Atrue%2C%22isLicenseFlow%22%3Afalse%2C%22isDomesticFlow%22%3Afalse%2C%22isCertificateOfNonImpediment%22%3Afalse%2C%22isRecordsRoom%22%3Afalse%2C%22isMarriageOfficiantRegistration%22%3Afalse%2C%22isPageLoad%22%3Afalse%2C%22isDateChanged%22%3Atrue%2C%22isWeekChanged%22%3Afalse%2C%22locationId%22%3A%220013d000003HJArAAO%22%2C%22selectedDate%22%3A%22${selectedDate}%22%2C%22selectedSlotId%22%3Anull%2C%22selectedSlotData%22%3A%22null%22%7D%2C%22cacheable%22%3Afalse%2C%22isContinuation%22%3Afalse%7D%7D%5D%7D&aura.context=%7B%22mode%22%3A%22PROD%22%2C%22fwuid%22%3A%222yRFfs4WfGnFrNGn9C_dGg%22%2C%22app%22%3A%22siteforce%3AcommunityApp%22%2C%22loaded%22%3A%7B%22APPLICATION%40markup%3A%2F%2Fsiteforce%3AcommunityApp%22%3A%22KbCmDBVbE10iCy1inwbbzA%22%2C%22COMPONENT%40markup%3A%2F%2Finstrumentation%3Ao11yCoreCollector%22%3A%22kA6gW5EadEh9qZBKZj4IqQ%22%7D%2C%22dn%22%3A%5B%5D%2C%22globals%22%3A%7B%7D%2C%22uad%22%3Afalse%7D&aura.pageURI=%2Fs%2FMarriageCeremony&aura.token=null`
+function generateData(selectedDate) {
+    return `message=%7B%22actions%22%3A%5B%7B%22id%22%3A%2289%3Ba%22%2C%22descriptor%22%3A%22aura%3A%2F%2FApexActionController%2FACTION%24execute%22%2C%22callingDescriptor%22%3A%22UNKNOWN%22%2C%22params%22%3A%7B%22namespace%22%3A%22%22%2C%22classname%22%3A%22SCHED_BookAppointmentController%22%2C%22method%22%3A%22getSlots%22%2C%22params%22%3A%7B%22isDeviceMobile%22%3Afalse%2C%22isCeremonyFlow%22%3Atrue%2C%22isLicenseFlow%22%3Afalse%2C%22isDomesticFlow%22%3Afalse%2C%22isCertificateOfNonImpediment%22%3Afalse%2C%22isRecordsRoom%22%3Afalse%2C%22isMarriageOfficiantRegistration%22%3Afalse%2C%22isPageLoad%22%3Afalse%2C%22isDateChanged%22%3Atrue%2C%22isWeekChanged%22%3Afalse%2C%22locationId%22%3A%220013d000003HJArAAO%22%2C%22selectedDate%22%3A%22${selectedDate}%22%2C%22selectedSlotId%22%3Anull%2C%22selectedSlotData%22%3A%22null%22%7D%2C%22cacheable%22%3Afalse%2C%22isContinuation%22%3Afalse%7D%7D%5D%7D&aura.context=%7B%22mode%22%3A%22PROD%22%2C%22fwuid%22%3A%222yRFfs4WfGnFrNGn9C_dGg%22%2C%22app%22%3A%22siteforce%3AcommunityApp%22%2C%22loaded%22%3A%7B%22APPLICATION%40markup%3A%2F%2Fsiteforce%3AcommunityApp%22%3A%22KbCmDBVbE10iCy1inwbbzA%22%2C%22COMPONENT%40markup%3A%2F%2Finstrumentation%3Ao11yCoreCollector%22%3A%22kA6gW5EadEh9qZBKZj4IqQ%22%7D%2C%22dn%22%3A%5B%5D%2C%22globals%22%3A%7B%7D%2C%22uad%22%3Afalse%7D&aura.pageURI=%2Fs%2FMarriageCeremony&aura.token=null`
+}
 
 let complete = false
 
+async function getMaxDate() {
+    const response = (await axios.post(api_url, generateData('2022-06-23'), {headers})).data
+    return response.actions[0].returnValue.returnValue.maxDate;
+}
+
 async function fetchTimeSlots() {
-    const response = (await axios.post(api_url, data, {headers})).data
+    const maxDate = await getMaxDate();
+    const response = (await axios.post(api_url, generateData(maxDate), {headers})).data
 
     const slotsAvailable = [];
 
